5080aad101bab8b638d8831ab75189868b135aa8
sankarebarri <sankarebarri_dev@yahoo.com>
2025-09-16 00:40:18 +0000

    chapter_1

---
+++ chapter_one/vector_class.py
@@ -0,0 +1,36 @@
+import math
+
+class Vector:
+    
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+
+    def __repr__(self):
+        return f'Vector({self.x!r}, {self.y!r})'
+    
+    def __abs__(self):
+        return math.hypot(self.x, self.y)
+    
+    def __bool__(self):
+        return bool(abs(self))
+    
+    def __add__(self, other):
+        x = self.x + other.x
+        y = self.y + other.y
+        return Vector(x, y)
+    
+    def __mul__(self, scalar):
+        return Vector(self.x * scalar, self.y * scalar)
+    
+
+v1 = Vector(1, 6)
+v2 = Vector(3, 9)
+
+print(v1 + v2)
+print(abs(v2))
+print(abs(v1))
+print(v1 * 3)
+# print(3 * v2)
+
+print(v1)
